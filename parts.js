628f0c633fe933e259c1a68d1835c0c882119b14
fabelavalon <118684863+fabelavalon@users.noreply.github.com>
2023-10-15 00:39:47 -0400

    added leonclaw and vipertail

--- parts.js
+++ parts.js
@@ -15,9 +15,11 @@ var blades = [
     {"name":"WizardArrow", "spin":"right", "weight":31.86, "height":0, "abbv":"WA", "wins":0, "losses":0, "draws":0, "id":4},
     {"name":"DranzerSprial", "spin":"right", "weight":27.82, "height":0, "abbv":"DrS", "wins":0, "losses":0, "draws":0, "id":5},
     {"name":"KnightLance", "spin":"right", "weight":33.25, "height":0, "abbv":"KL", "wins":0, "losses":0, "draws":0, "id":6},
-    {"name":"SharkEdge", "spin":"right", "weight":34.60, "height":0, "abbv":"SE", "wins":0, "losses":0, "draws":0, "id":7}//,
-    //{"name":"LeonClaw", "spin":"right", "weight":0.00, "height":0, "abbv":"LC", "wins":0, "losses":0, "draws":0, "id":8},
-    //{"name":"ViperTail", "spin":"right", "weight":0.00, "height":0, "abbv":"VT", "wins":0, "losses":0, "draws":0, "id":9}
+    {"name":"SharkEdge", "spin":"right", "weight":34.60, "height":0, "abbv":"SE", "wins":0, "losses":0, "draws":0, "id":7},
+    {"name":"LeonClaw", "spin":"right", "weight":31.40, "height":0, "abbv":"LC", "wins":0, "losses":0, "draws":0, "id":8},
+    {"name":"ViperTail", "spin":"right", "weight":34.55, "height":0, "abbv":"VT", "wins":0, "losses":0, "draws":0, "id":9}//,
+    //{"name":"DranDagger", "spin":"right", "weight":0.00, "height":0, "abbv":"DD", "wins":0, "losses":0, "draws":0, "id":10},
+    //{"name":"HellsChain", "spin":"right", "weight":0.00, "height":0, "abbv":"HC", "wins":0, "losses":0, "draws":0, "id":11}
     
 ];
 
@@ -26,9 +28,9 @@ var rachets = [
     {"name":"3-60", "weight":6.32, "height":60, "abbv":"3-60", "wins":0, "losses":0, "draws":0, "id":0},
     {"name":"3-80", "weight":7.10, "height":80, "abbv":"3-80", "wins":0, "losses":0, "draws":0, "id":1},
     {"name":"4-60", "weight":6.30, "height":60, "abbv":"4-60", "wins":0, "losses":0, "draws":0, "id":2},
-    {"name":"4-80", "weight":7.00, "height":80, "abbv":"4-80", "wins":0, "losses":0, "draws":0, "id":3}//,
-    //{"name":"5-60", "weight":0.00, "height":80, "abbv":"5-60", "wins":0, "losses":0, "draws":0, "id":4},
-    //{"name":"5-80", "weight":0.00, "height":80, "abbv":"5-80", "wins":0, "losses":0, "draws":0, "id":5}
+    {"name":"4-80", "weight":7.00, "height":80, "abbv":"4-80", "wins":0, "losses":0, "draws":0, "id":3},
+    {"name":"5-60", "weight":6.60, "height":60, "abbv":"5-60", "wins":0, "losses":0, "draws":0, "id":4},
+    {"name":"5-80", "weight":7.15, "height":80, "abbv":"5-80", "wins":0, "losses":0, "draws":0, "id":5}
 
 ];
 
@@ -39,8 +41,10 @@ var bits = [
     {"name":"Needle", "weight":2.03, "height":0, "abbv":"N", "wins":0, "losses":0, "draws":0, "id":2},
     {"name":"Taper", "weight":2.19, "height":0, "abbv":"T", "wins":0, "losses":0, "draws":0, "id":3},
     {"name":"High Needle", "weight":2.17, "height":0, "abbv":"HN", "wins":0, "losses":0, "draws":0, "id":4},
-    {"name":"Low Flat", "weight":2.15, "height":0, "abbv":"LF", "wins":0, "losses":0, "draws":0, "id":5}//,
-    //{"name":"Point", "weight":0.00, "height":0, "abbv":"P", "wins":0, "losses":0, "draws":0, "id":6},
-    //{"name":"Orb", "weight":0.00, "height":0, "abbv":"O", "wins":0, "losses":0, "draws":0, "id":7}
+    {"name":"Low Flat", "weight":2.15, "height":0, "abbv":"LF", "wins":0, "losses":0, "draws":0, "id":5},
+    {"name":"Point", "weight":2.20, "height":0, "abbv":"P", "wins":0, "losses":0, "draws":0, "id":6},
+    {"name":"Orb", "weight":2.02, "height":0, "abbv":"O", "wins":0, "losses":0, "draws":0, "id":7}//,
+    //{"name":"Rush", "weight":0.00, "height":0, "abbv":"R", "wins":0, "losses":0, "draws":0, "id":8},
+    //{"name":"High Taper", "weight":0.00, "height":0, "abbv":"HT", "wins":0, "losses":0, "draws":0, "id":9}
 
 ];
