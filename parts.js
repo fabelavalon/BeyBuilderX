f508caeae81541d31385d53cada371bf02885626
Fabel <118684863+fabelavalon@users.noreply.github.com>
2025-02-15 10:46:06 -0500

    added shelter drake

--- parts.js
+++ parts.js
@@ -1,5 +1,5 @@
 /*==========================================================*
- * BeyBuilder v1.1 for Beyblade X                           *
+ * BeyBuilder v1.3 for Beyblade X                           *
  * Author: Fabel                                            *
  * Copyright 2023-2025                                      *
  *==========================================================*/
@@ -68,8 +68,8 @@ var blades = [
     {"name":"DracielShield", "spin":"right", "weight":28.15, "height":0, "abbv":"DcS", "wins":0, "losses":0, "draws":0, "id":57},
     {"name":"ImpactDrake", "spin":"right", "weight":38.85, "height":0, "abbv":"ID", "wins":0, "losses":0, "draws":0, "id":58},
     {"name":"GhostCircle", "spin":"right", "weight":26.71, "height":0, "abbv":"GC", "wins":0, "losses":0, "draws":0, "id":59},
-    {"name":"GolemRock", "spin":"right", "weight":34.00, "height":0, "abbv":"GR", "wins":0, "losses":0, "draws":0, "id":60}//,
-    //{"name":"ShelterDrake", "spin":"right", "weight":0, "height":0, "abbv":"SD", "wins":0, "losses":0, "draws":0, "id":61}
+    {"name":"GolemRock", "spin":"right", "weight":34.00, "height":0, "abbv":"GR", "wins":0, "losses":0, "draws":0, "id":60},
+    {"name":"ShelterDrake", "spin":"right", "weight":32.62, "height":0, "abbv":"SD", "wins":0, "losses":0, "draws":0, "id":61}
     
 
     
@@ -97,8 +97,8 @@ var rachets = [
     {"name":"2-70", "weight":6.34, "height":7, "abbv":"2-70", "wins":0, "losses":0, "draws":0, "id":17},
     {"name":"3-85", "weight":4.79, "height":8.5, "abbv":"3-85", "wins":0, "losses":0, "draws":0, "id":18},
     {"name":"7-70", "weight":7.24, "height":7, "abbv":"7-70", "wins":0, "losses":0, "draws":0, "id":19},
-    {"name":"0-80", "weight":7.53, "height":8, "abbv":"0-80", "wins":0, "losses":0, "draws":0, "id":20}//,
-    //{"name":"7-80", "weight":0, "height":8, "abbv":"7-80", "wins":0, "losses":0, "draws":0, "id":21}
+    {"name":"0-80", "weight":7.53, "height":8, "abbv":"0-80", "wins":0, "losses":0, "draws":0, "id":20},
+    {"name":"7-80", "weight":7.82, "height":8, "abbv":"7-80", "wins":0, "losses":0, "draws":0, "id":21}
 
 ];
 
