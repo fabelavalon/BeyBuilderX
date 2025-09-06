a75cd289788ff920ed4b307c91e676cf9c739509
gregandcin <gregandcinbusiness@gmail.com>
2025-09-06 16:55:27 -0400

    feat: add eslint, Vite PWA plugin and scripts

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import { VitePWA } from "vite-plugin-pwa";
+
+export default {
+  server: {
+    port: 8080,
+  },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "BeyBuilderX",
+        short_name: "BeyBuilderX",
+        description: "Beyblade X Testing Web App",
+        theme_color: "#59298c",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+};
