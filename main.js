35fe11a59f480c95943ef6c5a7888f95dc190ccb
maave0 <1850819+maave0@users.noreply.github.com>
2026-08-11 21:00:44 -0400

    scale up popup

--- main.js
+++ main.js
@@ -2642,7 +2642,7 @@ const createBrowserPopup = async () => {
     // opening a blank popup gives us DOM control, avoiding same-origin issues when loading HTML with file://
     scoreOverlayWindow = window.open("", 
         "msgWindow", 
-        "width=600,height=300,top=200,left=200"  // forces a new window, set size, move away from corner
+        "width=1200,height=600,top=200,left=200"  // forces a new window, set size, move away from corner
     );
     scoreOverlayWindow.document.write(html);
     // wait a few milliseconds for the page to open, needs at least 50ms
