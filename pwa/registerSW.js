8cf19aae7a82b0dc2e14f3a811c9c9b08cb92a42
maave0 <1850819+maave0@users.noreply.github.com>
2026-08-24 18:11:28 -0400

    move pwa manifest to service worker

--- pwa/registerSW.js
+++ pwa/registerSW.js
@@ -1,3 +1,6 @@
+if (!location.protocol.startsWith('file:')) 
+    document.write(' <link rel="manifest" id="pwa-manifest" href="./pwa/manifest.webmanifest"> ');
+
 if ('serviceWorker' in navigator && !window.location.protocol.startsWith("file")) { 
     window.addEventListener('load', () => { 
         navigator.serviceWorker.register('/BeyBuilderX/serviceWorker.js', { scope: '/BeyBuilderX/' }) 
