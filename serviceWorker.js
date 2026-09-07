e39e6ba17c99d0566c872a3b842070998e32552c
maave0 <1850819+maave0@users.noreply.github.com>
2026-09-06 23:46:21 -0400

    migrate abbreviated bit names

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beybuilderx-cache-v2';
+const CACHE_NAME = 'beybuilderx-cache-v3';
 const PRECACHE_URLS = [
   'pwa/registerSW.js',
   // main application
@@ -11,6 +11,7 @@ const PRECACHE_URLS = [
   'migrations/backup.js',
   'migrations/migrate.js',
   'migrations/versions/001_stadium_vs_records.js',
+  'migrations/versions/002_full_bit_names.js',
   // themes
   'beybuilder.css',
   'theme-dark-purple.css',
